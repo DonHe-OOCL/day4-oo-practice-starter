b5a450bf8cb53bb8de05f8f0c13e6e23441fad77
HEDO <hedo@oocl.com>
2024-11-21 11:26:24 +0800

    [Java OOP] feat: AC3

--- src/afs/training/oo/Main.java
+++ src/afs/training/oo/Main.java
@@ -3,9 +3,15 @@ package afs.training.oo;
 public class Main {
     public static void main(String[] args) {
         Bus bus = new Bus("Cool Bus", 25, 5);
-        bus.seepUp();
+//        bus.seepUp();
 
         Truck truck = new Truck("Big Truck", 20, 2);
-        truck.seepUp();
+//        truck.seepUp();
+
+        Driver driver = new Driver(bus);
+        driver.driveBus();
+
+        driver = new Driver(truck);
+        driver.driveTruck();
     }
 }
